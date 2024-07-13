c9b8b2bca4afd9cc1e9ba1b174ea9b052a1d1a87
sagunsinghb <sagunsinghbaghel@gmail.com>
2024-07-13 17:45:44 +0530

    Init fake-store-api project
     - Add initial project for learning spring boot.

---
+++ src/test/java/com/dev/sagun/fakestoreapi/FakeStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.sagun.fakestoreapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeStoreApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
