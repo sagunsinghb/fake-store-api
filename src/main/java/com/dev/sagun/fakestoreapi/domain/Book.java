427c80cd1bc3d8f691ab8df1e988fbea8a68824a
sagunsinghb <sagunsinghbaghel@gmail.com>
2024-07-13 19:56:56 +0530

    Add Book entity. Closes #1.

---
+++ src/main/java/com/dev/sagun/fakestoreapi/domain/Book.java
@@ -0,0 +1,28 @@
+package com.dev.sagun.fakestoreapi.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String isbn;
+    private String author;
+
+    public Book(String title, String isbn, String author) {
+        this.title = title;
+        this.isbn = isbn;
+        this.author = author;
+    }
+
+}
